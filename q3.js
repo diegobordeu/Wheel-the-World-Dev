42898c95f86927961adb7c52a700f6b079bf430f
Diego Bordeu <diego@Diegos-MacBook-Pro-2.local>
2022-05-08 19:57:27 -0400

    wip/checkpint before huge refactor

--- q3.js
+++ q3.js
@@ -42,28 +42,31 @@ while not finished:
     direction *= -1
  */
 
-const moveTo = (pos, delta, matrix, response) => {
+const moveTo = (pos, delta, matrix, response, finished) => {
   pos = [pos[0] + delta[0], pos[1] + delta[1]];
   response.push(matrix[pos[0]][pos[1]]);
+  finished = false;
 }
 
 const canMove = (pos, delta, matrix, visited) => {
-  const [row, col] = pos;
-  if (row < 0 || row === matrix.length || col < 0 || col === matrix[0].length) {
+  const newRow = pos[0] + delta[0];
+  const newCol = pos[1] + delta[1];
+  if (newRow < 0 || newRow === matrix.length || newCol < 0 || newCol === matrix[0].length) {
     return false;
   }
-  if (visited[row][col]) return false;
+  if (visited[newRow][newCol]) return false;
   return true;
 }
 
 const doTheZigZagThing = (matrix) => {
   const initRow = new Array(matrix[0].length).fill(false);
   const visited = new Array(matrix.length).fill(initRow);
+  console.log(visited);
   const resolveTop = [(0, 1), (-1, 0)];
   const resolveBottom = [(-1, 0), (0, 1)];
   let direction = -1;
   const pos = [0, 0];
-  const delta = [1, 1];
+  let delta = [1, 1];
   const response = [];
   response.push(matrix[pos[0]][pos[1]]);
   
@@ -73,11 +76,25 @@ const doTheZigZagThing = (matrix) => {
     const resolver = direction === -1 ? resolveTop : resolveBottom;
     delta = [delta[0] * direction, delta[1] * direction]
     while (canMove(pos, delta, matrix, visited)) {
-      move(delta, array, finished);
+      move(pos, delta, matrix, response, finished);
     }
-
-
+    for (const movement of resolver) {
+      if (canMove(pos, movement, matrix, visited)) {
+        move(pos, movement, matrix, response, finished);
+        break;
+      }
+    }
+    direction *= -1
   }
-
+  return response;
 }
 
+const matrix = [
+  [1, 3, 4, 10],
+  [2, 5, 9, 11],
+  [6, 8, 12, 15],
+  [7, 13, 14, 16],
+]
+
+const res = doTheZigZagThing(matrix);
+console.log(matrix);
