8f27114b419fdab79bde47a71fe861539ad36cef
Diego Bordeu <diego@Diegos-MacBook-Pro-2.local>
2022-05-08 14:06:22 -0400

    testing/q1: main cases, todo: edge cases

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ const getTotalSpeed = (redShirtSpeeds, blueShirtSpeeds, fastest) => {
   return totalSum;
 }
 
+
 const red = [5, 5, 3, 9, 2];
 const blue = [3, 6, 7, 2, 1];
 
--- test/testQ1.js
+++ test/testQ1.js
@@ -8,6 +8,10 @@ const {
 
 describe('Testing For question Q1', () =>  {
   
+  /**
+   * sortAsc, sortDesc is not needed to test as it is a javascript native sorting approche
+   * i just did it to set up the testing env
+   */
   describe('sortAsc()', () => {
     it('(unit): happy path, expected outcome', () => {
       const outcome = sortAsc(8, 3)
@@ -21,5 +25,45 @@ describe('Testing For question Q1', () =>  {
       assert.equal(outcome, 3 - 8);
     });
   });
+
+
+  /**
+   * it suppose to be integration for calling sortDesc and sortAsc 
+   * It is more like a unit testing
+   * */
+  describe('getTotalSpeed()', () => {
+    it('(unit): happy path, fasted = TRUE question example', () => {
+      const red = [5, 5, 3, 9, 2];
+      const blue = [3, 6, 7, 2, 1];
+      const outcome = getTotalSpeed(red, blue, true);
+      assert.equal(outcome, 32, 'broken algorithm')
+    });
+
+    it('(unit): happy path, fasted = FALSE question example', () => {
+      const red = [5, 5, 3, 9, 2];
+      const blue = [3, 6, 7, 2, 1];
+      const outcome = getTotalSpeed(red, blue, false);
+      assert.equal(outcome, 25, 'broken algorithm')
+    });
+
+    it('(unit): hard path: plenty of number fasted = FALSE ', () => {
+      const red = [1, 3, 6, 4, 9, 5, 3, 2, 1, 0, 0, 0, 40];
+      const blue = [20, 1, 4, 7, 8, 2, 56, 1, 2, 0, 2, 0, 4];
+      const outcome = getTotalSpeed(red, blue, false);
+      assert.equal(outcome, 108, 'broken algorithm')
+    });
+
+    it('(unit): hard path: plenty of number fasted = FALSE ', () => {
+      const red = [1, 3, 6, 4, 9, 5, 3, 2, 1, 0, 0, 0, 40];
+      const blue = [20, 1, 4, 7, 8, 2, 56, 1, 2, 0, 2, 0, 4];
+      const outcome = getTotalSpeed(red, blue, true);
+      assert.equal(outcome, 169, 'broken algorithm')
+    });
+
+    it('(unit): edge case: empity string', () => {
+      const outcome = getTotalSpeed([], [], true);
+      assert.equal(outcome, 0, 'broken validation')
+    });
   
+  });
 });
